9ffe5842d1f51a8a0f8a72f58907057a0e0b17ff
Cinthiams <cinbertolo@gmail.com>
2022-12-07 20:26:24 -0300

    add funcionalidade get

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
-import { Observable, observable, subscribeOn } from 'rxjs';
+import { ServiceService } from './services/service.service';
 
 @Component({
   selector: 'app-root',
   template:  `
   <app-page></app-page>
-  <h1>{{ palavra }}<h1>
+  <br>
+  <h1>{{ palavra | json }}<h1>
+  <button (click)="consulta()">click aqui </button>
   <router-outlet></router-outlet>`
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  palavra!:any;
+  palavra!:any[];
   public e = "teste"
-  constructor( private http: HttpClient){}
-  ngOnInit(): void {
-    this.http.get<any>('http://localhost:2222/api/ok').subscribe(data =>{
-      this.palavra = data;
-    })
+  constructor(private Service:ServiceService){}
 
+  ngOnInit(): void {
+ 
+  
+  }
+  consulta(){
+    return this.Service.getAll().subscribe(resultado =>this.palavra = resultado);
   }
   
 }
--- Project/src/app/services/service.service.ts
+++ Project/src/app/services/service.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
-import { Observable, observable } from 'rxjs';
+import { HttpBackend, HttpClient, HttpErrorResponse, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
   private apiUrl = 'https://localhost:2222/ok'
-  constructor(private http: HttpClient) { }
-  getAll(): Observable<any> {
-    return this.http.get<any>(this.apiUrl)
-
+  constructor( private http: HttpClient) { }
+  
+  
+  getAll(): Observable<any[]>{
+    
+    return this.http.get<any[]>(`http://localhost:2222/api/ok`)
+    
+    //this.http.get<any>('http://localhost:2222/api/ok').subscribe(data =>{
+    //this.palavra = data;
 
   }
+  
 }
